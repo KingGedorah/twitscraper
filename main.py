06b30dcfb7541413160f5a76b851cb1f0bc02977
KingGedorah <naufal.zhafari@ui.ac.id>
2025-01-22 14:48:23 +0700

    add sleep interval and 01 scrape result

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import json
 import csv
 
 
-QUERY = '#02 since:2023-11-01 until:2024-02-14 lang:id'
+QUERY = '#01 since:2023-11-01 until:2024-02-14 lang:id'
 
 # Initialize client
 client = Client('en-US')
 
-async def main():
+async def TwikitScrape():
     
     # config = ConfigParser()
     # config.read('config.ini')
@@ -24,10 +24,11 @@ async def main():
     client.load_cookies('cookies.json')
     
     all_tweet_ids = set()
+    processed_count = 0
     
     tweets = await client.search_tweet(QUERY, 'Latest')
     
-    with open('02.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open('01.csv', 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['id', 'text', 'created_at', 'hashtags']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -36,6 +37,7 @@ async def main():
             for tweet in tweets:
                 if tweet.id not in all_tweet_ids:
                     all_tweet_ids.add(tweet.id)  # Add the tweet ID to the set
+                    processed_count += 1  # Increment the counter
 
                     # Write the tweet to the CSV file
                     writer.writerow({
@@ -45,13 +47,19 @@ async def main():
                         'hashtags': ', '.join(tweet.hashtags) if tweet.hashtags else '',
                     })
 
+                    # Sleep for 5 minutes after processing 200 tweets
+                    if processed_count % 100 == 0:
+                        print(f'{processed_count} tweets processed. Sleeping for 5 minutes...')
+                        csvfile.flush()  # Ensure all data is written to the file before pausing
+                        await asyncio.sleep(180) 
+
             # Check if there's a next batch of tweets
             if tweets.next:
                 tweets = await tweets.next()
             else:
                 break
 
-    print(f'Tweets saved to 02.csv')
+    print(f'Tweets saved to 01.csv')
 
 
-asyncio.run(main())
+asyncio.run(TwikitScrape())
